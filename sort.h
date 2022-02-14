cb8593aa8d0a8c21dc6723e2143c95c44261b768
Diego Monfort <diegomonfort12346@gmail.com>
2022-02-14 14:05:41 -0800

    [ADDED NEW FUNCTIONS]

--- sort.h
+++ sort.h
@@ -32,5 +32,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swapfro(listint_t *head, listint_t **list);
+void swapback(listint_t *head, listint_t **list);
 
 #endif
